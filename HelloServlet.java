47cf5d3b6deaeb932898559a19492fc0e4ce0fb8
sairohan <sairohan005@gmail.com>
2025-09-12 14:12:52 +0530

    Initial Feature

--- HelloServlet.java
+++ HelloServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-//This is Hospital management application
+//This is Hospital management application.
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
     @Override
